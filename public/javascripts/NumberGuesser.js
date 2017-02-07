f8b4946907c702fbec00fd62fc4c03c72b3777f0
sdweber422 <sdweber422@gmail.com>
2017-02-07 10:33:29 -0800

    Added tests and made review corrections

--- public/javascripts/NumberGuesser.js
+++ public/javascripts/NumberGuesser.js
@@ -1,27 +1,44 @@
 class NumberGuesser {
-    constructor() {
-    this.clear()
+  constructor() {
+  this.clear()
   }
-  attempts(guess) {
+  attempt(guess) {
     if(guess !== 'exit') {
      this.guesses.push(guess, this.compare(guess))
    }
   }
 
   clear() {
-    this.answer = Math.ceil(Math.random() * 100)
+    let newAnswer
+    do{
+      newAnswer = Math.ceil(Math.random() * 100)
+    }
+    while(this.answer === newAnswer)
+    this.answer = newAnswer
     this.guesses = []
   }
 
   compare(guess) {
-    if(guess === 'exit') {return}
-    guess = parseInt(guess)
-      if(isNaN(guess)) return 'Please enter a number, no letters accepted'
-      if(guess < 1 || guess > 100) 'Please enter a number between 1 and 100, including 1 or 100'
-      if(guess === this.answer) return 'You guessed correctly!'
-      if(guess < this.answer) return 'Your guess is lower than the computer\'s number'
-      if(guess > this.answer) return 'Your guess is higher than the computer\'s number'
+    if(guess === 'exit') {
+      return
     }
+    guess = parseInt(guess)
+    if(isNaN(guess)) {
+      return 'Please enter a number, no letters accepted'
+    }
+    if(guess < 1 || guess > 100) {
+      return 'Please enter a number between 1 and 100, including 1 or 100'
+    }
+    if(guess === this.answer) {
+       return 'You guessed correctly!'
+    }
+    if(guess < this.answer) {
+      return 'Your guess is lower than the computer\'s number'
+    }
+    if(guess > this.answer) {
+      return 'Your guess is higher than the computer\'s number'
+    }
+  }
 }
 
 module.exports = new NumberGuesser();
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const numberGuesser = require('../public/javascripts/NumberGuesser')
 /* GET home page. */
 router.get('/', function(request, response, next) {
   const { guess, exit } = request.query
-  if(guess == 'exit' || exit === 'exit') numberGuesser.clear()
+  if(guess === 'exit' || exit === 'exit') {
+    numberGuesser.clear()
+  }
+  numberGuesser.attempt(guess)
   response.render('index', { title: 'Express', attempts: numberGuesser.guesses });
 });
 
