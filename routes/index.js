fbd69d868157466436261b7afe0b0b00a377cfd4
sdweber422 <sdweber422@gmail.com>
2017-02-06 17:42:45 -0800

    Removed console.log

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const numberGuesser = require('../public/javascripts/NumberGuesser')
 router.get('/', function(request, response, next) {
   const { guess, exit } = request.query
   if(guess == 'exit' || exit === 'exit') numberGuesser.clear()
-  console.log('numberGuesser.attempts(guess)', numberGuesser.attempts(guess))
   response.render('index', { title: 'Express', attempts: numberGuesser.guesses });
 });
 
